9104b6c52b0a69acb4758fffc91ef24e53fb5b01
132yse <1533540012@qq.com>
2018-07-25 15:56:20 +0800

    初步调整架构

--- server/model/option.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-// 新建表模型
-const UserSchema = new Schema({
-  banner: {type: String, require: true}
-})
-
-module.exports = mongoose.model('Option', UserSchema)
