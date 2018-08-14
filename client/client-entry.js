ce3d303f6ca22f36982fddf845a248de86bd2efd
132yse <1533540012@qq.com>
2018-08-14 09:09:00 +0800

    修复bug

--- client/client-entry.js
+++ client/client-entry.js
@@ -6,7 +6,12 @@ const {app, router, store} = createApp()
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__)
 }
-
+router.beforeEach((to, from, next) => {
+  if (to.path) {
+    _hmt.push(['_trackPageview', '/#' + to.fullPath])
+  }
+  next()
+})
 
 router.onReady(() => {
   app.$mount('#app')
