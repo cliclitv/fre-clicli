fbfa329c8f41be03b2f4fd840b423be73a3a2f63
132yse <1533540012@qq.com>
2018-09-25 15:10:06 +0800

    日常提交

--- server/ssr-online.js
+++ server/ssr-online.js
@@ -2,6 +2,7 @@ const Router = require('koa-router')
 const path = require('path')
 const fs = require('fs')
 const axios = require('axios')
+
 const VueServerRenderer = require('vue-server-renderer')
 const clientManifest = require('../dist/vue-ssr-client-manifest.json')
 
@@ -16,6 +17,8 @@ const renderer = VueServerRenderer.createRenderer({
 
 // 导出路由
 const router = new Router()
+
+// 解析嘀哩嘀哩和halihali
 let out
 router.get('/v/', async ctx => {
   const start = ctx.query.url
@@ -50,6 +53,56 @@ router.get('/v/', async ctx => {
   ctx.body = out
 
 })
+let cid
+// 解析哔哩哔哩
+router.get('/bili/', async ctx => {
+  let start = ctx.query.url
+
+
+  start = start.replace('www.', 'm.')
+  await axios.get(start).then(res => {
+    let avid = res.data.match(/aid(\S*)cid/)
+    cid = res.data.match(/cid(\S*)cover/)
+    if (avid && cid) {
+      avid = avid[1].substring(2, 10)
+      cid = cid[1].substring(2, 10)
+    }
+    axios.get('https://bangumi.bilibili.com/player/web_api/v2/playurl/html5', {
+      params: {
+        cid,
+        season_type: 1,
+        bsource: ''
+      },
+      headers: {
+        Host: 'bangumi.bilibili.com',
+        Referer: start
+      }
+    }).then(res => {
+      out = res.data.src
+    })
+  })
+  if (out) {
+    ctx.body = `
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
+<div class="ep" style="height: 100%;width: 100%"></div>
+<script src="https://alicdn.imh3.cn/eplayer.js"></script>
+<script>
+new Eplayer(document.querySelector('.ep'),{
+  themeColor:'linear-gradient(to right,#0072ff ,#00e7ff)',
+  src:'${out}'
+})
+</script>
+`
+  }
+
+
+})
+
 
 router.get('*', async (ctx) => {
   ctx.type = 'html'
