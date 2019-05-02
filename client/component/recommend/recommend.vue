9c5d5f00c3a6cb484c33a213fcbee4d460f037ac
132yse <1533540012@qq.com>
2019-05-02 10:06:28 +0800

    暂时更换为jsdelibr

--- client/component/recommend/recommend.vue
+++ client/component/recommend/recommend.vue
@@ -52,7 +52,7 @@
         })
       },
       getAuthors() {
-        let page = Math.ceil(Math.random() * 18)
+        let page = Math.ceil(Math.random() * 16)
         getUsers('up', page, 5).then(res => {
           this.authors = res.data.users
         })
