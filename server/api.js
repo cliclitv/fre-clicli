21fe7eef5c3afbb40eab7f3b6d7046f484f4ca51
NaccOll <wuyoubin0504@gmail.com>
2019-08-01 22:14:27 +0800

    refactor: add transaction about /get/pv (#15)

--- server/api.js
+++ server/api.js
@@ -21,65 +21,65 @@ const knex = require('knex')({
 router.get('/upload/auth', async ctx => {
   let filename = ctx.query.fname
   let vn = ctx.query.rname
-  let params = `/auth/upload.json?filename=${encodeURIComponent(filename)}&vn=${encodeURIComponent(vn)}`
+  let params = `/auth/upload.json?filename=${encodeURIComponent(
+    filename
+  )}&vn=${encodeURIComponent(vn)}`
   let accessToken = util.getAccessToken(params)
-  await axios.get(`https://api.dogecloud.com${params}`, {
-    headers: {
-      Host: 'api.dogecloud.com',
-      Authorization: `TOKEN ${accessToken}`
-    }
-  }).then(res => {
-    if (res.data.code === 200) {
-      ctx.body = {
-        code: 0,
-        uploadToken: res.data.data.uploadToken
+  await axios
+    .get(`https://api.dogecloud.com${params}`, {
+      headers: {
+        Host: 'api.dogecloud.com',
+        Authorization: `TOKEN ${accessToken}`
       }
-    }
-  })
+    })
+    .then(res => {
+      if (res.data.code === 200) {
+        ctx.body = {
+          code: 0,
+          uploadToken: res.data.data.uploadToken
+        }
+      }
+    })
 })
 
 router.get('/get/pv', async function addPv(ctx) {
-  const pid = parseInt(ctx.query.pid);
-  let pv = await knex.transaction(trx => {
-    return knex('pv')
+  const pid = parseInt(ctx.query.pid)
+  let pvNumber = await knex.transaction(async trx => {
+    let pvs = await knex('pv')
       .transacting(trx)
       .forUpdate()
       .where('pid', pid)
       .select('pv')
-      .limit(1);
-  });
-  let pvNumber;
-  if (!pv.length) {
-    let initialValue = Math.floor(Math.random() * 100);
-    await knex('pv').insert({ pid, pv: initialValue });
-    pvNumber = initialValue;
-  } else {
-    await knex('pv')
-      .where('pid', pid)
-      .update({ pv: pv[0].pv + 1 });
-    pvNumber = pv[0].pv;
-  }
+      .limit(1)
+    if (!pvs.length) {
+      let initialValue = 1
+      await trx('pv').insert({ pid, pv: initialValue })
+      return initialValue
+    } else {
+      await trx('pv')
+        .where('pid', pid)
+        .update({ pv: pvs[0].pv + 1 })
+      return pvs[0].pv
+    }
+  })
+
   ctx.body = {
     code: 0,
     pid,
     pv: pvNumber
-  };
-});
+  }
+})
 
 router.get('/jx/', Jx.default)
 // router.get('/hcy/list', Hcy.getList)
-
 
 app.use(async (ctx, next) => {
   ctx.set('Access-Control-Allow-Origin', '*')
   await next()
 })
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods())
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(5000, '0.0.0.0', () => {
   console.log('api started on 5000')
 })
-
