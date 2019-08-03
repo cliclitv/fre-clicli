04c0433f11eb97ba2c678c02d97276adcf46023a
132yse <1533540012@qq.com>
2019-08-03 18:12:03 +0800

    增加页面配置文件

---
+++ clicli.manifest.js
@@ -0,0 +1,13 @@
+(function () {
+  window.adLink = `https://dwz.cn/jc0psWg2`
+  window.gonggao = `c站更换 cdn，招募投稿，加群：857500387`
+})()
+
+
+var _hmt = _hmt || []
+(function () {
+  var hm = document.createElement("script")
+  hm.src = "https://hm.baidu.com/hm.js?8fc8aa49cffa79e5b92078fd15951dd1"
+  var s = document.getElementsByTagName("script")[0]
+  s.parentNode.insertBefore(hm, s)
+})()
