c0df120e92ffaf1681b58a97f57a062224652786
132yse <1533540012@qq.com>
2019-11-05 22:17:54 +0800

    更换公告

--- clicli.manifest.js
+++ clicli.manifest.js
@@ -4,7 +4,7 @@
     'https://p.pstatp.com/origin/fe3d0000a45a85140221': 'http://down2.guopan.cn/andl/dda.php?appid=119290&cid=215989&t=1566826799',
     'https://p.pstatp.com/origin/fe5a0000c97c3f1db7b3': 'http://down2.guopan.cn/andl/dda.php?appid=120184&cid=215989&t=1566822551'
   };
-  window.gonggao = 'c站番剧搬运小分队持续招募，群号：399395607';
+  window.gonggao = 'c站官方邮箱更换为 admin@clicli.us，版权，合作欢迎骚扰~';
 })();
 
 
