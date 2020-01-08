82a2926ce5a9ae634996e893d624b376f2a47e32
132yse <1533540012@qq.com>
2020-01-08 20:14:43 +0800

    日常提交

--- src/widget/tab/index.js
+++ src/widget/tab/index.js
@@ -12,7 +12,7 @@ export default function Tab() {
   return (
     <nav className="tab">
       <ul className="wrap">
-        <a href="/"><li>主站</li></a>
+        <a href="/"><li class="active">主站</li></a>
         {Object.keys(obj).map(key => (<a href={`${clink}/play/gv${obj[key]}`}>
             <li className='item'>{key}</li>
           </a>
