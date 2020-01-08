acde5ad1ea6cfa746da78ac22cf029b4e063d552
132yse <1533540012@qq.com>
2020-01-08 14:36:09 +0800

    日常提交

--- src/public/js/const.js
+++ src/public/js/const.js
@@ -1,3 +1,3 @@
 export const clink = 'https://www.clicli.me'
-export const alink = 'https://admin.clicli.me'
+export const alink = 'https://admin.clicli.us'
 export const isDev = process.env.NODE_ENV === 'development'
