6c5ab2f854b2bc17101066f07abf2015ee1b113d
132yse <1533540012@qq.com>
2020-01-09 15:28:52 +0800

    彻底搞定搜索

--- src/widget/search/index.js
+++ src/widget/search/index.js
@@ -1,11 +1,17 @@
-import { h } from 'fre'
+import { h, useState } from 'fre'
+import { clink } from 'public/js/const'
 import './index.styl'
 
 export default function Search(props) {
+  const [word, setWord] = useState('')
+  const inputWord = e => {
+    setWord(e.target.value)
+    console.log(word)
+  }
   return (
     <div className='search'>
-      <input type='text' placeholder='搜索一下下菊花又不会坏(⊙o⊙)…' />
-      <a>
+      <input type='text' placeholder='搜索一下下菊花又不会坏(⊙o⊙)…' onInput={inputWord} />
+      <a href={`${clink}/search/${word}`} target='_blank'>
         <i class='icon-font icon-search'></i>
       </a>
     </div>
